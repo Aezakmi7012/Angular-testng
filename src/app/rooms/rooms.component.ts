cd014cd8363261442386f57be1f312bb3e9b34e7
ASWIN <rbai8913@gmail.com>
2023-12-25 13:33:04 +0530

    changes

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Room, RoomList } from './rooms';
 
 @Component({
@@ -6,7 +6,7 @@ import { Room, RoomList } from './rooms';
   templateUrl: './rooms.component.html',
   styleUrl: './rooms.component.scss',
 })
-export class RoomsComponent {
+export class RoomsComponent implements OnInit {
   hotelName = 'Hilton Hotel';
 
   numberofRooms = 10;
@@ -19,39 +19,43 @@ export class RoomsComponent {
     bookedRooms: 5,
   };
 
-  roomList: RoomList[] = [
-    {
-      roomNumber:1,
-      roomType: 'Deluxe Room',
-      amenities: 'AC,WIFI,TV.BATHROOM,KITCHEN',
-      price: 500,
-      photos: 'NAhh fam',
-      checkinTime: new Date('11-Nov-2021'),
-      checkoutTime: new Date('12-Nov-2021'),
-      rating:4.5
-    },
-    {
-      roomNumber:2,
-      roomType: 'Deluxe Room',
-      amenities: 'AC,WIFI,TV.BATHROOM,KITCHEN',
-      price: 1000,
-      photos: 'NAhh fam',
-      checkinTime: new Date('11-Nov-2021'),
-      checkoutTime: new Date('12-Nov-2021'), 
-      rating:3.6
-    },
-    {
-      roomNumber:3,
-      roomType:'Private Suite',
-      amenities:"AC,WIFI,TV.BATHROOM,KITCHEN",
-      price:15000,
-      photos:'NAhh fam',
-      checkinTime: new Date('11-Nov-2021'),
-      checkoutTime: new Date('12-Nov-2021'),
-      rating:3.2
-  
-    }
-  ];
+  roomList: RoomList[] = [];
+  constructor() {}
+
+  ngOnInit(): void {
+    this.roomList = [
+      {
+        roomNumber: 1,
+        roomType: 'Deluxe Room',
+        amenities: 'AC, WIFI, TV, BATHROOM, KITCHEN',
+        price: 500,
+        photos: 'NAhh fam',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('12-Nov-2021'),
+        rating: 4.5,
+      },
+      {
+        roomNumber: 2,
+        roomType: 'Deluxe Room',
+        amenities: 'AC, WIFI, TV, BATHROOM, KITCHEN',
+        price: 1000,
+        photos: 'NAhh fam',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('12-Nov-2021'),
+        rating: 3.6,
+      },
+      {
+        roomNumber: 3,
+        roomType: 'Private Suite',
+        amenities: 'AC, WIFI, TV, BATHROOM, KITCHEN',
+        price: 15000,
+        photos: 'NAhh fam',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('12-Nov-2021'),
+        rating: 3.2,
+      },
+    ];
+  }
 
   toggle() {
     this.hideRooms = !this.hideRooms;
